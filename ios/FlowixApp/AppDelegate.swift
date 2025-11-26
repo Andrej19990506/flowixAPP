491dcb943b394df8564ff87ba0299dd9c0c7f810
Andrejnikolaevich1999 <malahow.andrej2014@yandex.ru>
2025-11-27 02:55:58 +0700

    refactor: Simplify AppDelegate setup for React Native
    - Remove unnecessary ReactNativeDelegate and RCTReactNativeFactory
    - Streamline the application launch process with RCTRootView
    - Update JavaScript bundle URL handling for both debug and release modes

--- ios/FlowixApp/AppDelegate.swift
+++ ios/FlowixApp/AppDelegate.swift
@@ -1,48 +1,32 @@
 import UIKit
 import React
-import React_RCTAppDelegate
-import ReactAppDependencyProvider
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
+  var bridge: RCTBridge!
 
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
+    let jsCodeLocation: URL
 
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
+    #if DEBUG
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+    jsCodeLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle")!
+    #endif
+
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "FlowixApp", initialProperties: nil, launchOptions: launchOptions)
+    
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
 
     window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "FlowixApp",
-      in: window,
-      launchOptions: launchOptions
-    )
-
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+    
     return true
-  }
-}
-
-class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
-
-  override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
   }
 }
